ed1557c941c5a26ea8c1d1acd6c8c157dd3b17ed
MachaSreenath <sreenathshree9@gmail.com>
2025-02-24 18:47:55 +0530

    roboshop shared library changes

--- vars/pipelineDecision.groovy
+++ vars/pipelineDecision.groovy
@@ -1,5 +1,7 @@
 #!groovy
 
+// here nodejsVM, javaVM and nodejsEKS are the different environments, in catalogue jenkins file we pass the values there which environment we want and here it selects and executes, means this shared library is acting as to support in different environments and not to write the pipeines from the scratch. 
+
 def decidePipeline(Map configMap){
     application = configMap.get("application")
     switch(application) {
